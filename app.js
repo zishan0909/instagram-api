993ab339486b2c378f1b762cc37c8bdb9480988f
plagiarism2012 <sonu.deo2012@gmail.com>
2023-03-09 12:57:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const instagramGetUrl = require("instagram-url-direct");
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/media', async (req, res)=>{
+    try{
+        const response = await instagramGetUrl(req.body.url);
+        res.send(response);
+    }catch(err){
+        res.send({message: err});
+    }
+});
+
+app.listen(port, ()=>{
+    console.log('Server is running on port ' + port);
+});
