948cb1ed8c7ac65d5bca8ed6ab1deb84b3384d87
tanay388 <tanay.deo388@gmail.com>
2023-05-25 10:36:50 +0530

    Api: changed to working status

--- app.js
+++ app.js
@@ -26,25 +26,25 @@ app.post("/media", async (req, res) => {
   }
 });
 
-app.get("/social/insta", async (req, res) => {
-  const stringData = req.query.stringData;
-  if (!stringData) {
-    res.status(400).send("stringData parameter is missing");
-    return;
-  }
-  const apiUrl = "https://igram.live/wp-json/igram/video-data/";
-  const body = { url: stringData, token: "" };
-  try {
-    const response = await axios.post(apiUrl, body);
-    const filteredMedia = response.data.medias.filter(
-      (media) => media.quality !== "480p" && media.quality !== "360p"
-    );
-    const urlList = filteredMedia.map((media) => media.url);
-    res.send({ message: "Success", url_list: urlList });
-  } catch (err) {
-    res.send({ message: err.message, url_list: [] });
-  }
-});
+// app.get("/social/insta", async (req, res) => {
+//   const stringData = req.query.stringData;
+//   if (!stringData) {
+//     res.status(400).send("stringData parameter is missing");
+//     return;
+//   }
+//   const apiUrl = "https://igram.live/wp-json/igram/video-data/";
+//   const body = { url: stringData, token: "" };
+//   try {
+//     const response = await axios.post(apiUrl, body);
+//     const filteredMedia = response.data.medias.filter(
+//       (media) => media.quality !== "480p" && media.quality !== "360p"
+//     );
+//     const urlList = filteredMedia.map((media) => media.url);
+//     res.send({ message: "Success", url_list: urlList });
+//   } catch (err) {
+//     res.send({ message: err.message, url_list: [] });
+//   }
+// });
 
 // app.get("/social/insta", async (req, res) => {
 //   const stringData = req.query.stringData;
@@ -90,48 +90,48 @@ app.get("/social/insta", async (req, res) => {
 //   }
 // });
 
-// app.get("/social/insta", async (req, res) => {
-//   let stringData = req.query.stringData;
-//   if (!stringData) {
-//     res.status(400).send("stringData parameter is missing");
-//     return;
-//   }
-//   const requ = {
-//     url: stringData,
-//   };
-//   try {
-//     // const url = req.body.url;
-//     if (stringData[8] === "i") {
-//       stringData = stringData.replace("instagram.com", "www.instagram.com");
-//     }
-//     console.log(stringData.split("/"));
-//     console.log(stringData.split("/")[5]);
-//     const response = await axios.post(
-//       stringData.split("/")[4]
-//         ? "https://dowmate.com/api/allinone/"
-//         : "https://dowmate.com/api/instadp/",
-//       requ
-//     );
-//     console.log(response.data);
-//     const arrayList = [];
-//     if (stringData.split("/")[4]) {
-//       if (response.data.data.Type.includes("Carousel")) {
-//         console.log(response.data.data.media);
-//         response.data.data.media.map((item, indx) => {
-//           arrayList.push(item);
-//         });
-//       } else {
-//         arrayList.push(response.data.data.media);
-//       }
-//     } else {
-//       arrayList.push(response.data.img);
-//     }
-//     console.log(arrayList);
-//     res.send({ url_list: arrayList });
-//   } catch (err) {
-//     res.send({ message: err });
-//   }
-// });
+app.get("/social/insta", async (req, res) => {
+  let stringData = req.query.stringData;
+  if (!stringData) {
+    res.status(400).send("stringData parameter is missing");
+    return;
+  }
+  const requ = {
+    url: stringData,
+  };
+  try {
+    // const url = req.body.url;
+    if (stringData[8] === "i") {
+      stringData = stringData.replace("instagram.com", "www.instagram.com");
+    }
+    console.log(stringData.split("/"));
+    console.log(stringData.split("/")[5]);
+    const response = await axios.post(
+      stringData.split("/")[4]
+        ? "https://dowmate.com/api/allinone/"
+        : "https://dowmate.com/api/instadp/",
+      requ
+    );
+    console.log(response.data);
+    const arrayList = [];
+    if (stringData.split("/")[4]) {
+      if (response.data.data.Type.includes("Carousel")) {
+        console.log(response.data.data.media);
+        response.data.data.media.map((item, indx) => {
+          arrayList.push(item);
+        });
+      } else {
+        arrayList.push(response.data.data.media);
+      }
+    } else {
+      arrayList.push(response.data.img);
+    }
+    console.log(arrayList);
+    res.send({ message: "success", url_list: arrayList });
+  } catch (err) {
+    res.send({ message: err, url_list: [] });
+  }
+});
 
 app.get("/social/insta/media", async (req, res) => {
   const mediaUrl = req.query.mediaUrl;
